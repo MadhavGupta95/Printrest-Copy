00bb54f46cbf3ad9d2d071e43ae09662a69e5478
Madhav Gupta <guptamadhav2005@gmail.com>
2025-04-15 21:49:13 +0530

    added more utils inside utils->auth->index.js

--- server/src/utils/auth/index.js
+++ server/src/utils/auth/index.js
@@ -1,16 +1,53 @@
 import bcrypt from "bcryptjs";
+import logger from "../logger";
+import jwt from "jsonwebtoken";
 
 const hashPassword = async (password) => {
   try {
     const salt = bcrypt.genSalt(10);
     const hashedPass = bcrypt.hash(password, salt);
-    return hashedPass
+    return hashedPass;
   } catch (error) {
     logger.error(error.message);
     return null;
   }
 };
 
+const verifyPassword = async (password, hashedPassword) => {
+  try {
+    const verifyPass = await bcrypt.compare(password, hashedPassword);
+    return verifyPass;
+  } catch (error) {
+    logger.error(error.message);
+    return null;
+  }
+};
+
+const generateAuthToken = (payload) => {
+  return jwt.sign(payload, process.env.AUTH_JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+const verifyAuthToken = (token) => {
+  return jwt.verify(token, process.env.AUTH_JWT_SECRET);
+};
+
+const generateResetToken = (payload) => {
+  return jwt.sign(payload, process.env.RESET_PASSWORD_JWT_SECRET, {
+    expiresIn: 300,
+  });
+};
+
+const verifyResetToken = (token) => {
+  return jwt.verify(token, process.env.RESET_PASSWORD_JWT_SECRET);
+};
+
 export {
-    hashPassword,
-}
+  hashPassword,
+  verifyPassword,
+  generateAuthToken,
+  verifyAuthToken,
+  generateResetToken,
+  verifyResetToken,
+};
