ac3a8878d47ccd1aa805a33530a148d22101a694
Madhav Gupta <guptamadhav2005@gmail.com>
2025-04-22 21:46:51 +0530

    setting up react

---
+++ server/src/models/schemas/Comment.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    text: {
+      type: String,
+      required: [true, "comment text is required"],
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Comment", CommentSchema);
