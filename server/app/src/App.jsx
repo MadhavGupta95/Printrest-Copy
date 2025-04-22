8e05475bb59e77870e6e3847a6c88b2c2fa56ebd
Madhav Gupta <guptamadhav2005@gmail.com>
2025-04-22 22:58:15 +0530

    toast setup and interceptor

--- server/app/src/App.jsx
+++ server/app/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router";
+import "./index.css";
+import React from "react";
+import Home from "./components/Home";
+import { Toaster, toast } from "react-hot-toast";
 function App() {
-
   return (
     <>
-      
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ server/app/src/utils/axios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 5000,
+  headers: {},
+});
+
+//? interceptors stops something in the middle, they are like middlewares.
+
+instance.interceptors.response.use(
+  (response) => response.data, //? here, whenever the response will come, it will return the response's data.
+  (error) => {
+    toast.error(error.response?.data?.message || "something went wrong");
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+//? we can also make an interceptor for the request, but we dont need it in our case { it has the same syntax just replace response with the request}
+
+export default instance;
