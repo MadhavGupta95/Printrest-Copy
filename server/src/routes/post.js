9129e9faa2ddb3ca1fb6f94379641cebf78ce9c3
Madhav Gupta <guptamadhav2005@gmail.com>
2025-04-23 22:18:12 +0530

    post routes done

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -25,6 +25,12 @@ router.post(
         ...validatedBody,
         user: userId,
       });
+      await post.save();
+      return res.json({
+        success: true,
+        message: "Post created successfully",
+        data: post,
+      });
     } catch (error) {
       console.log(error.message);
       return res.json({
@@ -37,8 +43,97 @@ router.post(
 );
 
 // Todo : Like a post
-// Todo : Dislike a post
-// Todo : Delete a post
-// Todo : Update a post
+router.post("/:id", withAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const likeAPost = await Post.findById(id);
+    const { _id: userId } = req.user;
 
+    if (!likeAPost) {
+      return res.json({
+        success: false,
+        message: "Post not found (like).",
+        data: null,
+      });
+    }
+
+    await Post.findByIdAndUpdate(id, {
+      $addToSet: {
+        likes: userId,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Post liked!",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.json({
+      success: false,
+      message: "Error in liking a post",
+      data: null,
+    });
+  }
+});
+
+// Todo : Delete a post
+router.post("/:id", withAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletePost = await Post.findById(id);
+    if (!deletePost) {
+      return res.json({
+        success: false,
+        message: "Post not found (delete)",
+        data: null,
+      });
+    }
+    await Post.findByIdAndUpdate(id, {
+        deleted: true,
+    });
+    return res.json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.json({
+      success: false,
+      message: "Error with deleting a post",
+    });
+  }
+});
+
+
+// Todo : Update a post
+router.patch("/:id", withAuth, async(req, res)=>{
+  try {
+    const {id} = req.params
+    const updatePost = await Post.findById(id)
+    const updatedPost = req.body
+    if(!updatePost){
+      return res.json({
+        success : false,
+        message : "Post not found (update)"
+      })
+    }
+
+    const UpdateThePost = await Post.findByIdAndUpdate(id, updatedPost,{
+      new : true
+    })
+
+    return res.json({
+      success: true,
+      message: "Post updated successfully",
+      data: UpdateThePost,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.json({
+      success : false,
+      message : "Error in updating a post."
+    })
+  }
+})
 export default router;
